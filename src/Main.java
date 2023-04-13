f06fa266ec3e6dba591b09c2ccd32a42dd3c6cca
gefy-q <Andr.Sonya@mail.ru>
2023-04-13 13:01:05 +0300

    description

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+/*
+Считывает файл по полученному пути и заполняет коллекцию, если файла нет - возвращает ошибку
+После вызывает view-menu где происходят основные действия
+ */
+
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -12,12 +17,13 @@ import model.DragonCharacter;
 import representations.json.JsonCollectionControllerRepr;
 import representations.json.JsonDragonRepr;
 import ui.View;
-
+import java.util.Scanner;
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class Main {
@@ -27,18 +33,47 @@ public class Main {
             return;
         }
 
-        final Path dataFile = Paths.get(args[0]);
+        final Path dataFile = Paths.get(args[0]); // "C:\\Users\\ASUS\\OneDrive\\Рабочий стол\\учеба\\прога\\352.json"
         CollectionController controller = new ArrayListController();
 
         try {
             JsonCollectionControllerRepr.read(new InputStreamReader(new FileInputStream(dataFile.toString())), controller);
         } catch (IOException e) {
             System.err.println("Error: Cannot read data file.");
+            readFile(controller);
         }
 
         BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter outputWriter = new BufferedWriter(new OutputStreamWriter(System.out));
         View view = new View(dataFile, controller);
         view.run(inputReader, outputWriter);
+
+
+    }
+
+    private static void readFile(CollectionController controller) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            String name = readName(scanner);
+            JsonCollectionControllerRepr.read(new InputStreamReader(new FileInputStream(name.toString())), controller);
+        } catch (IOException e) {
+            System.err.println("Error: Cannot read data file.");
+            readFile(controller);
+        }
+    }
+
+    private static String readName(Scanner scanner) throws IOException {
+        while (true) {
+            String name;
+            System.err.println("Enter path to data file: ");
+            while (scanner.hasNextLine()) {
+                name = scanner.nextLine().trim();
+                if (name.isEmpty()) {
+                    System.err.println("It cannot be empty");
+                    continue;
+                }
+                return name;
+            }
+        }
     }
 }
